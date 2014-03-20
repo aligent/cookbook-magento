f9ef6dadc5d5dc5487604de837a864a09c1d42eb
Luke Mills <luke@aligent.com.au>
2014-03-20 17:35:40 +1030

    Added the ability to set arbitrary server parameters for nginx.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,6 +10,7 @@ default[:magento][:db][:database] = "magentodb"
 default[:magento][:db][:username] = "magentouser"
 default[:magento][:admin][:email] = "webmaster@localhost"
 default[:magento][:admin][:user] = "admin"
+default[:magento][:server_params] = {}
 
 # Default FastCGI Process
 default[:magento][:nginx][:fastcgi_process] = "unix:/var/run/php-fpm-www.sock"
@@ -21,4 +22,5 @@ set_unless[:magento][:admin][:password] = secure_password
 
 default['n98magerun']['revision'] = 'master'
 default['modman']['revision'] = 'master'
+
 
